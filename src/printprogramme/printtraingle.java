9e50bccfab53048201bda6c11eb551ceca993e88
Henyshah1 <henyshah.qa@gmail.com>
2023-10-08 20:50:41 +0100

    new

--- src/printprogramme/printtraingle.java
+++ src/printprogramme/printtraingle.java
@@ -2,11 +2,11 @@ package printprogramme;
 
 public class printtraingle {
     public static void main(String [] args){
-        System.out.println("  *  ");
-        System.out.println("  **  ");
-        System.out.println("  *** ");
-        System.out.println(" **** ");
-        System.out.println("*****");
-        System.out.println("******");
+        System.out.println("     *  ");
+        System.out.println("    * *  ");
+        System.out.println("   * * * ");
+        System.out.println("  * * * * ");
+        System.out.println(" * * * * *");
+        System.out.println("* * * * * *");
     }
 }
